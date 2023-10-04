789cadf6b74717bb7390dabe13b2ade18b310e3a
SageKyle <ominyipaulo@gmail.com>
2023-10-04 14:05:34 +0100

    add animation to primary button utility component

--- src/utils/PrimaryBtn.jsx
+++ src/utils/PrimaryBtn.jsx
@@ -2,11 +2,12 @@ import { Link } from 'react-router-dom'
 
 export default function PrimaryBtn({ text, link, extraStyles }) {
 	return (
-		<button
-			type="button"
-			className={`bg-gradient capitalize cursor-pointer px-6 py-3 text-lg font-semibold rounded lg:ml-3 hover:bg-primary transition-all active:translate-y-2 ${extraStyles}`}
+		<Link
+			role="button"
+			className={`w-fit bg-gradient text-white capitalize cursor-pointer px-6 py-3 text-lg font-semibold rounded lg:ml-3 hover:bg-primary hover:bg-blend-soft-light ease-in-out hover:duration-300 hover:transition-colors active:translate-y-2 ${extraStyles}`}
+			to={link}
 		>
-			<Link to={link}>{text}</Link>
-		</button>
+			{text}
+		</Link>
 	)
 }
